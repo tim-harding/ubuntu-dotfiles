583c08080857f0165334943dcef5d0a7a282233a
Tim Harding <tim@timharding.co>
2022-01-14 12:02:28 -0800

    Modifying starship colors

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,9 @@
 if status is-interactive
-	alias ls="exa --long --all --grid --git --icons --header --no-user --no-permissions"
+	alias ls="exa --long --all --git --icons --header"
+	alias smerge="/mnt/c/Program\ Files/Sublime\ Merge/smerge.exe"
 	starship init fish | source
 	set -Ux EDITOR nvim
 end
 
 fish_add_path /home/tim/personal/21/11/WSL2-Linux-Kernel/tools/perf
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/tim/.ghcup/bin # ghcup-env
