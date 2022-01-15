d3a3cf90cd0003c07c1d4266ed5c75cc7433f610
Tim Harding <tim@timharding.co>
2022-01-14 20:36:03 -0800

    Updated exa command

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-	alias ls="exa --long --all --git --icons --header"
+	alias ls="exa --long --all --git --icons --header --no-permissions --no-user --time-style=long-iso --group-directories-first --level=2"
 	alias smerge="/mnt/c/Program\ Files/Sublime\ Merge/smerge.exe"
 	starship init fish | source
 	set -Ux EDITOR nvim
