03e2a439e11f9c9440de0daa50e6d236f51e856e
Tim Harding <tim@timharding.co>
2022-12-07 19:49:43 -0800

    Updates to fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,23 @@
 # Note: wslview to open files in the default application
 if status is-interactive
-	alias ls="exa --long --all --git --icons --header --no-permissions --no-user --time-style=long-iso --group-directories-first --level=2"
-	alias smerge="/mnt/c/Program\ Files/Sublime\ Merge/smerge.exe"
-	starship init fish | source
 	set -Ux EDITOR nvim
 	set fish_greeting
-	fish_add_path /mnt/c/Windows
-	fish_add_path ~/personal/22/04/tex_creator
+
+	abbr gs git switch
+	abbr ga git add .
+	abbr gc git commit
+
+	alias ls="exa --long --all --git --icons --header --no-permissions --no-user --time-style=long-iso --group-directories-first --level=2"
+	alias smerge="/mnt/c/Program\ Files/Sublime\ Merge/smerge.exe"
+	alias explorer="/mnt/c/Windows/explorer.exe"
+	alias tex_creator="~/personal/22/04/tex_creator/tex_init.fish"
+
 	fish_add_path /home/linuxbrew/.linuxbrew/bin
 	fish_add_path ~/miscellaneous/software/zig-linux-x86_64-0.10.0
+
+	set --global tide_right_prompt_items cmd_duration newline time 
+	fish_vi_key_bindings
 end
 
-fish_add_path /home/tim/personal/21/11/WSL2-Linux-Kernel/tools/perf
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/tim/.ghcup/bin # ghcup-env
-
-# The next line updates PATH for the Google Cloud SDK.
+# Updates PATH for the Google Cloud SDK.
 if [ -f '/home/tim/miscellaneous/google-cloud-sdk/path.fish.inc' ]; . '/home/tim/miscellaneous/google-cloud-sdk/path.fish.inc'; end
